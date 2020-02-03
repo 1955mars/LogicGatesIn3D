29a452ca9dea6b5edee4836f69582570779cfaf7
Mansoor Patan <1955mars@gmail.com>
2020-02-02 23:25:08 -0500

    Nand Gate in place

---
+++ Assets/Scripts/Nand.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nand : MonoBehaviour
+{
+    public GameObject gateBody;
+    public GameObject inputConnA;
+    public GameObject inputConnB;
+    public GameObject outputConn;
+
+    public Material wireOnMat;
+    public Material wireOffMat;
+
+    Wire inputWireA;
+    Wire inputWireB;
+    Wire outputWire;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputWireA = inputConnA.GetComponent<Wire>();
+        inputWireB = inputConnB.GetComponent<Wire>();
+        outputWire = outputConn.GetComponent<Wire>();
+    }
+
+    public void SetInputs(bool vol)
+    {
+        inputWireA.SetVoltage(vol);
+    }
+
+    public bool NandGate(bool inputA, bool inputB)
+    {
+        inputWireA.SetVoltage(inputA);
+        inputWireB.SetVoltage(inputB);
+        outputWire.SetVoltage(!(inputWireA.GetVoltage() & inputWireB.GetVoltage()));
+        return outputWire.GetVoltage();
+    }
+
+}
---
+++ Assets/Scripts/Voltage.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Voltage : MonoBehaviour
+{
+
+    public Wire[] wires;
+
+    public bool isHigh;
+    public Material[] mats;
+
+    MeshRenderer meshR;
+    bool prevState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshR = GetComponent<MeshRenderer>();
+        prevState = !isHigh;
+        SetVoltageOnWires();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(prevState != isHigh)
+        {
+            meshR.material = mats[isHigh?1:0];
+            prevState = isHigh;
+            SetVoltageOnWires();
+        }
+
+    }
+
+    public bool GetInput()
+    {
+        return isHigh;
+    }
+
+    void SetVoltageOnWires()
+    {
+        foreach(Wire w in wires)
+        {
+            w.SetVoltage(isHigh);
+        }
+    }
+}
